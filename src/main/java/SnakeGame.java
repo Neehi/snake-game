f4a791e3bcddf4bf7cf585e70d91724c647031a1
Neehi <nick.snape@hotmail.com>
2020-09-19 04:59:37 +0100

    Add logging

--- src/main/java/SnakeGame.java
+++ src/main/java/SnakeGame.java
@@ -1,7 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SnakeGame {
 
+    private static final Logger logger = LoggerFactory.getLogger(SnakeGame.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.info("Hello world!");
     }
 
 }
